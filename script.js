fc7e070067d62a998873f015dbcca339354e692f
Joshua77 <46546654+Joshua77@users.noreply.github.com>
2022-06-23 14:53:55 +0100

    Updated string.js
    String Manipulation (Embedding of variables)

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ console.log(firstName)
 console.log(myCountry)
 console.log(myHeight)
 
-alert('My name is Joshua')
-alert('My Country is Nigeria')
-alert('My height is 1.5m')
+//alert('My name is Joshua')
+//alert('My Country is Nigeria')
+//alert('My height is 1.5m')
+
+let fullDetails = `My name is ${firstName} Height is ${myHeight} and Country is ${myCountry}`
+alert(fullDetails)
