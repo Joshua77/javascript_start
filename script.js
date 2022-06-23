9921e9e5748396a3f9a9ce50af646a630fcb245f
Joshua77 <46546654+Joshua77@users.noreply.github.com>
2022-06-23 12:57:47 +0100

    The HTML and JS files for the task

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let firstName = "Joshua"
+let myCountry = "Nigeria"
+let myHeight = "1.5"
+
+console.log(firstName)
+console.log(myCountry)
+console.log(myHeight)
+
+alert('My name is Joshua')
+alert('My Country is Nigeria')
+alert('My height is 1.5m')
